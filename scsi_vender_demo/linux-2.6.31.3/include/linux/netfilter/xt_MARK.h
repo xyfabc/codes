0fb7a7e3a088fce22d1c26169257b963d2b46d94
xyfabc <xyfabcde@126.com>
2018-09-07 14:30:27 +0800

    xyfabc

--- scsi_vender_demo/linux-2.6.31.3/include/linux/netfilter/xt_MARK.h
+++ scsi_vender_demo/linux-2.6.31.3/include/linux/netfilter/xt_MARK.h
@@ -1,27 +1,16 @@
-#ifndef _XT_MARK_H_target
-#define _XT_MARK_H_target
+#ifndef _XT_MARK_H
+#define _XT_MARK_H
 
 #include <linux/types.h>
 
-/* Version 0 */
-struct xt_mark_target_info {
-	unsigned long mark;
+struct xt_mark_info {
+    unsigned long mark, mask;
+    __u8 invert;
 };
 
-/* Version 1 */
-enum {
-	XT_MARK_SET=0,
-	XT_MARK_AND,
-	XT_MARK_OR,
-};
-
-struct xt_mark_target_info_v1 {
-	unsigned long mark;
-	__u8 mode;
-};
-
-struct xt_mark_tginfo2 {
+struct xt_mark_mtinfo1 {
 	__u32 mark, mask;
+	__u8 invert;
 };
 
-#endif /*_XT_MARK_H_target */
+#endif /*_XT_MARK_H*/
